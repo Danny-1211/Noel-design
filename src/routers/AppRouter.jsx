b8ecb4a93cfe28ffb369bbbc34ade49be79cc37f
danny <danny21316@gmail.com>
2025-12-04 21:30:41 +0800

    feat: add blog pages version1 & add blog router

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import Collection from "../pages/Collection";
-
+import Blog from "../pages/Blog";
 const router = createBrowserRouter(
     [
         {
@@ -11,6 +11,7 @@ const router = createBrowserRouter(
             children: [
                 { index: true, element: <Home /> },
                 { path: 'collection', element: <Collection /> },
+                { path: 'blog', element: <Blog /> },
             ],
         },
     ]
